3495ce5ac7e8de3715fbeeadd2400e0602e5145d
Verfenders <verfenders@gmail.com>
2023-05-11 21:43:48 +0300

    context added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,9 @@ import Spinner from "../spinner/Spinner";
 import Navbar from "../navbar/Navbar";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Alert from "../alert/Alert";
+import AlertState from "../../context/alert/AlertState";
+import FirebaseState from "../../context/firebase/FirebaseState";
 
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
@@ -12,20 +15,28 @@ const MainPage = lazy(() => import("../pages/MainPage"));
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
+    <FirebaseState>
+      <AlertState>
+        <Router>
+          <div className="app">
+            <Navbar />
 
-        <Suspense fallback={Spinner}>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
-        </Suspense>
+            <Suspense fallback={Spinner}>
+              <div className="container pt-4">
+                <Alert />
+                <Routes>
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="*" element={<Page404 />} />
+                </Routes>
+              </div>
+            </Suspense>
+          </div>
+        </Router >
+      </AlertState>
+
+    </FirebaseState>
 
 
-      </div>
-    </Router >
 
   );
 }
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Form from "../form/Form";
+import Notes from "../notes/Notes";
+
+
 
 const MainPage = () => {
+    const notes = new Array(3)
+        .fill("check")
+        .map((_, i) => ({ id: i, title: `Note ${i + 1}` }))
 
-    return (<>
-        <h1>Main</h1>
-    </>)
+    return (
+
+        <Fragment>
+            <Form />
+            <hr />
+            <Notes notes={notes} />
+        </Fragment>
+    )
 
 }
 
